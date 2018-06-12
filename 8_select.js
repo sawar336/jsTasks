15fabb807bd69f619cbc8f2155b7e77e7a1af978
Sasha Gereta <sawar336@gmail.com>
2018-06-12 17:48:25 +0300

    commit #4

--- 8_select.js
+++ 8_select.js
@@ -5,14 +5,13 @@ var d1 = $.Deferred(),
     s2 = document.getElementById('second');
 
 s1.onchange = function() {
-    d1.resolve;
+    d1.resolve();
 };
 
 s2.onchange = function() {
-    d2.resolve;
+    d2.resolve();
 };
 
-d1.done(function() { alert('d1'); });
 $.when(d1, d2).done(function() {
     rez.innerHTML = +s1.value + +s2.value;
 })
