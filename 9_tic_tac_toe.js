f881cb9865142ae377b284c4a51bd25110cb5ad7
Sasha Gereta <sawar336@gmail.com>
2018-06-20 04:15:29 +0300

    commit #5

--- 9_tic_tac_toe.js
+++ 9_tic_tac_toe.js
@@ -1,115 +1,139 @@
-var playField = document.getElementById('play_field'),
+;(function () {
+    var playField = document.getElementById('play_field'),
+    cleanBtn = document.getElementById('clean'),
+    xSize= document.getElementById('x_size'),
+    ySize= document.getElementById('y_size'),
     arr = document.getElementsByClassName('col'), 
     smallField = 3,
-    winerSymb;
+    winerSymb,
+    statusArr = [];
 
-function arrToMatrix(arr, sizeOfField) {
-    var rezArr = [],
-        v = 0;
-    
-    for(i = 0; i < sizeOfField; i++){
-        rezArr[i] = [];
-        for(var j = 0; j < sizeOfField; j++){
-        rezArr[i][j] = arr[v++];
+    function arrToMatrix(arr, sizeOfField) {
+        var rezArr = [],
+            v = 0;
+
+        for(i = 0; i < sizeOfField; i++){
+            rezArr[i] = [];
+            for(var j = 0; j < sizeOfField; j++){
+            rezArr[i][j] = arr[v++];
+            }
         }
-    }
-    return rezArr;
-};
+        return rezArr;
+    };
 
-elemsMatrix = arrToMatrix(arr, smallField);
+    elemsMatrix = arrToMatrix(arr, smallField);
 
-function isLine(symb) {
-    var cols,
-        rows;
-    
-    for(col = 0; col < smallField; col++) {
-        cols = 1;
-        rows = 1;
-        for (row = 0; row < smallField; row++) {
-            cols &= (elemsMatrix[col][row].innerHTML == symb);
-			rows &= (elemsMatrix[row][col].innerHTML == symb);
+    function isLine(symb) {
+        var cols,
+            rows;
+
+        for(col = 0; col < smallField; col++) {
+            cols = 1;
+            rows = 1;
+            for (row = 0; row < smallField; row++) {
+                cols &= (elemsMatrix[col][row].classList.contains(symb));
+                rows &= (elemsMatrix[row][col].classList.contains(symb));
+            }
+            if (cols || rows) {
+                winerSymb = symb;
+                return true;
+            } 
         }
-        if (cols || rows) {
+
+        return false;
+    };
+
+    function isDiagonal(symb) {
+        var rightD = 1,
+            leftD = 1;
+
+        for (el = 0; el < smallField; el++) {
+            rightD &= (elemsMatrix[el][el].classList.contains(symb));
+            leftD &= (elemsMatrix[smallField - el - 1][el].classList.contains(symb));
+        }
+
+        if (rightD || leftD) {
             winerSymb = symb;
             return true;
         } 
-    }
-    
-	return false;
-};
 
-function isDiagonal(symb) {
-    var rightD = 1,
-        leftD = 1;
-    
-    for (el = 0; el < smallField; el++) {
-        rightD &= (elemsMatrix[el][el].innerHTML == symb);
-        leftD &= (elemsMatrix[smallField - el - 1][el].innerHTML == symb);
+        return false;
     }
 
-    if (rightD || leftD) {
-        winerSymb = symb;
-        return true;
-    } 
-    
-    return false;
-}
+    function isADraw() {
+        var elem = 1;
 
-function isADraw() {
-    var elem = 1;
-    
-    for(col = 0; col < smallField; col++) {
-        for (row = 0; row < smallField; row++) {
-            elem &= (elemsMatrix[col][row].innerHTML !== "");
-        } 
-    }
-    if (elem) {
-        return true;
-    }
-    
-	return false;
-}
-
-
-function checkWin() {
-    if (isLine('x') || isLine('0') || isDiagonal('x') || isDiagonal('0')) {
-        alert('winer ' + winerSymb);
-        return true;
-    } else if (isADraw()) {
-        alert('draw');
-        return true;
-    }
-    
-    return false;
-}
-
-function finishTheGame() {
-    current = true;
-    if (checkWin()) {
-        for (i = 0; i < arr.length; i++) {
-            arr[i].innerHTML = "";
-            arr[i].classList.remove('null');
+        for(col = 0; col < smallField; col++) {
+            for (row = 0; row < smallField; row++) {
+                elem &= (elemsMatrix[col][row].innerHTML !== "");
+            } 
         }
-    }
-}
+        if (elem) {
+            return true;
+        }
 
-function insertSymbols() {
-    var current = true;
-    return function (e) {
-        if (e.target.className == 'col' && e.target.innerHTML != 'x' && e.target.innerHTML != '0') {
-            if (current) {
-                e.target.innerHTML = 'x';
-                current = false;
-            } else {
-                e.target.innerHTML = '0';
-                e.target.classList.add('null');
-                current = true;
+        return false;
+    }
+
+
+    function checkWin() {
+        if (isLine('cross') || isLine('null') || isDiagonal('cross') || isDiagonal('null')) {
+            alert('winer: ' + winerSymb);
+            return true;
+        } else if (isADraw()) {
+            alert('draw');
+            return true;
+        }
+    
+        return false;
+    }
+
+    function finishTheGame() {
+        status = true;
+        if (checkWin()) {
+            for (i = 0; i < arr.length; i++) {
+                arr[i].innerHTML = "";
+                arr[i].classList.remove('null');
+                arr[i].classList.remove('cross');
             }
-            finishTheGame();
+            
+            statusArr = [];
         }
     }
-};
 
-
-
-playField.addEventListener('click', insertSymbols());
+    function insertSymbols() {
+        var status;
+        return function (e) {
+            if (e.target.className == 'col' && !e.target.classList.contains('cross') && !e.target.classList.contains('null')) {
+                if (status) {
+                    e.target.classList.add('cross');
+                    e.target.innerHTML = 'x';
+                    status = false;
+                } else {
+                    e.target.classList.add('null');
+                    e.target.innerHTML = '0';
+                    status = true;
+                }
+                statusArr.push(e.target);
+                localStorage.foo = JSON.stringify(statusArr);
+                finishTheGame();
+            }
+        }
+    };
+    
+    function clen() {
+        var lastItem = statusArr[statusArr.length - 1];
+        lastItem.innerHTML = "";
+        lastItem.classList.remove('null');
+        lastItem.classList.remove('cross');
+        statusArr.pop();
+    }
+    
+    function saveStatus() {
+        
+    }
+    
+    playField.addEventListener('click', insertSymbols());
+    cleanBtn.addEventListener('click', clen);
+    
+})();
